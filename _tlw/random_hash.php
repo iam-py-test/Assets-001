c0b2a36e84ad6fd7f082ca2f50973a8fbeb0b8e6
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-08 09:55:06 -0400

    Update random_hash.php

--- _tlw/random_hash.php
+++ _tlw/random_hash.php
@@ -1,14 +1,16 @@
 <?php
 echo "<!DOCTYPE html>";
 echo "<title>Random test (try 2)</title>";
+#get the random
 $random = $_GET["random"];
 echo "<p>Random: $random</p>";
 $options = [ 
-							'cost' => 12, 
-						]; 
-						
+'cost' => 12, 
+]; 
+#hash the random number					
 $hashed = password_hash($random,PASSWORD_BCRYPT,$options);
 echo "<p>Hashed: $hashed</p>";
+# display if it worked
 $verified = password_verify($random,$hashed);
 echo "<p>Verified: $verified</p>";
 ?>
