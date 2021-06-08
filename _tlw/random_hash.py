017fd43697c25bd0b052cba012dbe23fcfd5a380
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-08 10:52:37 -0400

    Create random_hash.py

---
+++ _tlw/random_hash.py
@@ -0,0 +1,11 @@
+"""Note: this requires bcrypt to be installed"""
+import bcrypt
+password = input("Enter a password: ")
+bpassword = password.encode()
+#https://pypi.org/project/bcrypt/
+hashed = bcrypt.hashpw(bpassword,bcrypt.gensalt())
+print(hashed)
+if bcrypt.checkpw(password, hashed):
+  print("It Matches!")
+else:
+  print("It Does not Match :(")
