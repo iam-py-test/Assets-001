6cbc4520f6ca57e3e4018723d1062ef6f11ec23d
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-23 18:57:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/// this is from https://github.com/uBlock-user/uBO-Scriptlets/commit/3d1f48573749ac85b20031f78e0d5f7c7bb0f3af#
+
+
 /// cookie-set.js
 /// alias cs.js
 // example.com##+js(cs, name, value, age)
